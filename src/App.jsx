f8635140ea3e31add432f9d29d9ae63766341e55
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2025-12-31 04:06:18 +0600

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
